d0ef2c03ffc46ab01afd9cd249c7a6f9845f4ad8
shree243 <83566295+shree243@users.noreply.github.com>
2021-05-08 00:02:22 +0530

    Update seventh_7.java

--- assignment1/seventh_7.java
+++ assignment1/seventh_7.java
@@ -1,10 +1,15 @@
 	import java.util.Scanner;
+
 	
 public class seventh_7{
+	
 	public static void main (String[] args){
 		
 		Scanner Input= new Scanner(System.in);
-System.out.println("enter the value for table");
-int num=Input.nextInt();//8
-for (int i= 1; i<=10; i++){
-	System.out.printf("%d*%d=%d\n", num*i);
+		
+		System.out.println("enter the value");
+ 		int num=Input.nextInt();
+		for (int i= 1; i<=10; i++){
+	
+		System.out.printf("%d*%d=%d\n", num*i);
+}}
