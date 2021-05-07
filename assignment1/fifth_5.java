8ea42578fe48348dc008851363438087be1b6efe
shree243 <83566295+shree243@users.noreply.github.com>
2021-05-07 23:56:17 +0530

    Update fifth_5.java

--- assignment1/fifth_5.java
+++ assignment1/fifth_5.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
+
        public class fifth_5{
+	       
        public static void main(String[] args){
 	   Scanner Input = new Scanner (System.in);
 	   System.out.println("First number"); // 25 numnber 
@@ -7,5 +9,5 @@ import java.util.Scanner;
 	   System.out.println("Second numeber"); // 5 number 
 	   int num2= Input.nextInt();
 	   int num3=num1*num2;
-	   System.out.println("The product of 2 number is "+num3);// 125 product 
+	   System.out.println("The product of 2 number is "+num3); 
 }}
