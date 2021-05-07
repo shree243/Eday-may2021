fd5b02990e630e94d069f2ab7d5cc9287d5e0d3f
shree243 <83566295+shree243@users.noreply.github.com>
2021-05-08 00:01:04 +0530

    Update sixth_6.java

--- assignment1/sixth_6.java
+++ assignment1/sixth_6.java
@@ -1,22 +1,26 @@
 import java. util.Scanner;
 	
+
 	public class sixth_6{
+		
 		
 		public static void main(String[] args){
 			
+			
 			Scanner Input= new Scanner(System.in);
 			
-			System.out.println("input first number ");
+			System.out.println("input 1 number ");
 			int num1= Input.nextInt();
-			System.out.println("input second number");
+			System.out.println("input 2 number");
 			int num2= Input.nextInt();
 			int num3=num1+num2;
-			System.out.println("addition is "+num3);
+			System.out.println("add is "+num3);
 			int num4=num1-num2;
-			System.out.println("substract is "+num4);
+			System.out.println("sub is "+num4);
 			int num5=num1*num2;
-		System.out.println("multiple is "+num5);
+		        System.out.println("multi is "+num5);
 			int num6= num1/num2;
-			System.out.println("division is "+num6);
+			System.out.println("div is "+num6);
 			int num7=num1%num2;
 			System.out.println("mod is "+num7);
+		}}
