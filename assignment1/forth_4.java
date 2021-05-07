8d04b38e87f810980a4e71bbba6b122673a6f1b4
shree243 <83566295+shree243@users.noreply.github.com>
2021-05-07 23:57:57 +0530

    Update forth_4.java

--- assignment1/forth_4.java
+++ assignment1/forth_4.java
@@ -1,14 +1,15 @@
 public class forth_4{
 	    public static void main (String[] args){
+		    
 			int a,b, c, d;
 			
 		a=-5+8*6;
 		b= (55+9)%9;
 		c=20+-3*5/8;
 		d=5+15/3*2-8%3;
-		System.out.println("The value of a = "+a);
-		System.out.println("The value of b = "+b);
-		System.out.println("The value of c = "+c);
-		System.out.println("The value of d= "+d);
+		System.out.println("The value of a  = "+a);
+		System.out.println("The value of b  = "+b);
+		System.out.println("The value of c  = "+c);
+		System.out.println("The value of d  = "+d);
 		
 		
